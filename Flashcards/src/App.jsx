74078743e7166b1acf0df8111ffd7fdc3f09160f
Mohammad Zafar <mzcoder7@gmail.com>
2023-04-27 09:33:24 +0100

    Some styling

--- Flashcards/src/App.jsx
+++ Flashcards/src/App.jsx
@@ -1,36 +1,37 @@
 import './App.css';
-// import * as React from 'react';
 import { useNavigate } from 'react-router-dom';
 
 function App() {
 	const navigate = useNavigate();
-	const handleClick = () => navigate('/quizzes');
+
 	const handleCssClick = () => navigate('/CSS');
 	const handleJsClick = () => navigate('/JS');
 	const handleReactClick = () => navigate('/React');
 
 	return (
-		<>
+		<div className='container'>
 			<img
 				src='./src/assets/logo.png'
 				alt='SoC company logo'
 				width={400}
 				height={400}
+				className='logo'
 			/>
-			<h1>Welcome to CodeCards</h1>
-			<button type='button' onClick={handleClick}>
-				Quizzes
-			</button>
-			<button type='button' onClick={handleCssClick}>
-				CSS
-			</button>
-			<button type='button' onClick={handleJsClick}>
-				JavaScript
-			</button>
-			<button type='button' onClick={handleReactClick}>
-				React
-			</button>
-		</>
+			<div className='text-container'>
+				<h1 className='title'>Welcome to CodeCards</h1>
+			</div>
+			<div className='button-container'>
+				<button className='button' onClick={handleCssClick}>
+					CSS
+				</button>
+				<button className='button' onClick={handleJsClick}>
+					JavaScript
+				</button>
+				<button className='button' onClick={handleReactClick}>
+					React
+				</button>
+			</div>
+		</div>
 	);
 }
 
