4e9d90385f4b0ebadb96eb42c2aaed00937eb509
mia downe <miadowne@yahoo.com>
2023-04-27 09:24:35 +0100

    the audio calls

--- Flashcards/src/topics/CSS.jsx
+++ Flashcards/src/topics/CSS.jsx
@@ -1,11 +1,12 @@
 import './topics.css';
-
-import song from '../topics/music/DanielVeesey-SonataNo_19inGMinor_Op_49_No_1 _ I_Andante.mp3';
-import song2 from '../topics/music/Vassily_Primakov_piano-Felix_Mendelssohn_Songs_Without_Words.mp3';
-import song3 from '../topics/music/MusicianStringTrio.mp3';
-
+import AudioPlayer from './AudioPlayer';
 import { useState } from 'react';
 import ReactCardFlip from 'react-card-flip';
+
+// import song from '../topics/music/DanielVeesey-SonataNo_19inGMinor_Op_49_No_1 _ I_Andante.mp3';
+// import song2 from '../topics/music/Vassily_Primakov_piano-Felix_Mendelssohn_Songs_Without_Words.mp3';
+// import song3 from '../topics/music/MusicianStringTrio.mp3';
+
 
 export default function CSS() {
 	const [currentCardIndex, setCurrentCardIndex] = useState(0);
@@ -31,6 +32,7 @@ export default function CSS() {
 
 	return (
 		<>
+		<AudioPlayer/>
 			<header>
 				<button>Go back to homepage</button>
 			</header>
